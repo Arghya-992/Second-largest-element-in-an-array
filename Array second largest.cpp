6e3654ed35114459523b05ba0ad024200d88d830
Arghya-992 <76256630+Arghya-992@users.noreply.github.com>
2021-05-09 23:21:03 +0530

    Add files via upload

--- Array second largest.cpp
+++ Array second largest.cpp
@@ -1,27 +1,41 @@
 #include<stdio.h>
 int main(){
-	int n;
-	int a;
-	int b;
-	
-	printf("\n Enter the size of the array");
-	scanf("%d", &n);
-	
-	int array[n];
-	
-	printf("\n Enter the elements of the array:");
-	for(int i=0; i < n; i++){
-		scanf("%d", &array[i]);
-		for(int i=0; i < n; i++){
-			for(a=i + 1; a < n; a++){
-				if(array[i] < array[a])
-				{ b = array[i];
-				  array[i] = array[a];
-				  array[a] = b;
-				}
+
+int n;
+int a;//first maximum
+int b; //second maximum
+
+printf("\n Enter the size of the array:");
+scanf("%d", &n);
+
+int array[n];
+
+printf("Enter elements in the array: ");
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+
+a=b;
+
+for(int i=0; i<n; i++)
+    {
+        if(array[i] > a)
+        {
+            
+            b = a;
+            a = array[i];
+        }
+        else if(array[i] > b && array[i] < a)
+        {
+           
+            b = array[i];
         }
     }
+    
+    //printf("First largest = %d\n", max1);
+    printf("Second largest element is= %d", b);
+
+    return 0;
 }
-                printf("\n The second largest elemet in the array is %d", b);
-            return 0;
-}
+
